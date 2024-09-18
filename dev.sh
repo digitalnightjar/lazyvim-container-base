8806edfc7f125fd757c4495a2ffd06ed9657cc86
digitalnightjar <digitalnightjar@outlook.com>
2024-09-18 23:57:29 +0100

    Create dev.sh

---
+++ dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p ./dev_volume/local/share/nvim
+mkdir -p ./dev_volume/local/state/nvim
+mkdir -p ./dev_volume/cache/nvim
+podman run --rm -it \
+  -v ~/.ssh/:/root/.ssh:ro \
+  -v ./dev_volume/local/share/nvim:/local/share/nvim \
+  -v ./dev_volume/local/state/nvim:/local/state/nvim \
+  -v ./dev_volume/cache/nvim:/root/.cache/nvim \
+  -v ~/.gitconfig:/root/.gitconfig \
+  digitalnightjar/lazyvim-container-base:latest \
+  $1
